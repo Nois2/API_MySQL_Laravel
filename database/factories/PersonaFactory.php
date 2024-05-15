bb39e80d1e6e3659697cee85cd4e616063b330ae
Nois2 <103067863+Nois2@users.noreply.github.com>
2024-05-14 23:10:49 -0600

    Funciona /listarPersonas

--- database/factories/PersonaFactory.php
+++ database/factories/PersonaFactory.php
@@ -3,19 +3,12 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Persona>
  */
-class UserFactory extends Factory
+class PersonaFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
     /**
      * Define the model's default state.
      *
@@ -34,15 +27,5 @@ class UserFactory extends Factory
             'Correo' => $this->faker->correo(),
             'Direccion' => $this->faker->direccion(),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
